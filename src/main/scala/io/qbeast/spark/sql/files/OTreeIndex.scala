61b179c0ed215a90326ee45b858fd37f87058541
osopardo1 <paolapardoat@gmail.com>
2021-10-19 11:31:56 +0200

    Move and add methods for processing Expressions

--- src/main/scala/io/qbeast/spark/sql/files/OTreeIndex.scala
+++ src/main/scala/io/qbeast/spark/sql/files/OTreeIndex.scala
@@ -8,12 +8,11 @@ import io.qbeast.spark.model.{Point, QuerySpace, QuerySpaceFromTo, RangeValues}
 import io.qbeast.spark.sql.qbeast
 import io.qbeast.spark.sql.utils.State
 import io.qbeast.spark.sql.utils.TagUtils
-import org.apache.spark.sql.catalyst.expressions
-import org.apache.spark.sql.catalyst.expressions.{Expression, Literal}
+import io.qbeast.spark.sql.utils.QbeastExpressionUtils._
+import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.delta.Snapshot
 import org.apache.spark.sql.delta.actions.AddFile
 import org.apache.spark.sql.delta.files.{TahoeFileIndex, TahoeLogFileIndex}
-import org.apache.spark.sql.types.IntegerType
 
 /**
  * FileIndex to prune files
@@ -31,81 +30,18 @@ case class OTreeIndex(index: TahoeLogFileIndex)
 
   private val qbeastSnapshot = qbeast.QbeastSnapshot(snapshot)
 
-  /**
-   * Analyzes the data filters from the query
-   * @param dataFilters filters passed to the relation
-   * @return min max weight (default Integer.MINVALUE, Integer.MAXVALUE)
-   */
-  private def extractDataFilters(
-      dataFilters: Seq[Expression]): (Seq[Expression], Seq[Expression]) = {
-    dataFilters.partition(e => e.children.head.prettyName.equals("qbeast_hash"))
-  }
-
-  // TODO this ugly implementation is to test if it works
-
-  private def hasColumnReferences(attr: Expression, columnName: String): Boolean = {
-    val nameEquality = spark.sessionState.analyzer.resolver
-    attr.references.forall(r => nameEquality(r.name, columnName))
-  }
-
-  private def extractRangeFilters(
-      dataFilters: Seq[Expression],
-      dimensionColumns: Seq[String]): (Point, Point) = {
-
-    val fromTo = dimensionColumns.map(columnName => {
-
-      val columnFilters = dataFilters
-        .filter(hasColumnReferences(_, columnName))
-
-      val from = columnFilters
-        .collectFirst {
-          case expressions.GreaterThanOrEqual(_, Literal(m, _)) => m
-          case expressions.EqualTo(_, Literal(m, _)) => m
-        }
-        .getOrElse(Int.MinValue)
-        .asInstanceOf[Int]
-        .doubleValue()
-
-      val to = columnFilters
-        .collectFirst { case expressions.LessThan(_, Literal(m, _)) => m }
-        .getOrElse(Int.MaxValue)
-        .asInstanceOf[Int]
-        .doubleValue()
-
-      (from, to)
-
-    })
-
-    val from = fromTo.map(_._1)
-    val to = fromTo.map(_._2)
-    (Point(from.toVector), Point(to.toVector))
-  }
-
-  private def extractWeightRange(filters: Seq[Expression]): (Weight, Weight) = {
-    val min = filters
-      .collectFirst { case expressions.GreaterThanOrEqual(_, Literal(m, IntegerType)) =>
-        m.asInstanceOf[Int]
-      }
-      .getOrElse(Int.MinValue)
-
-    val max = filters
-      .collectFirst { case expressions.LessThan(_, Literal(m, IntegerType)) =>
-        m.asInstanceOf[Int]
-      }
-      .getOrElse(Int.MaxValue)
-
-    (Weight(min), Weight(max))
-  }
+  private val indexedCols = qbeastSnapshot.indexedCols
 
   override def matchingFiles(
       partitionFilters: Seq[Expression],
       dataFilters: Seq[Expression]): Seq[AddFile] = {
 
-    val (qbeastDataFilters, tahoeDataFilters) = extractDataFilters(dataFilters)
+    val (qbeastDataFilters, tahoeDataFilters) = extractDataFilters(dataFilters, indexedCols)
     val tahoeMatchingFiles = index.matchingFiles(partitionFilters, tahoeDataFilters)
 
     val (minWeight, maxWeight) = extractWeightRange(qbeastDataFilters)
-    val files = sample(minWeight, maxWeight, tahoeMatchingFiles, dataFilters)
+    val (from, to) = extractQueryRange(qbeastDataFilters, indexedCols)
+    val files = sample(minWeight, maxWeight, from, to, tahoeMatchingFiles)
 
     files
   }
@@ -122,16 +58,15 @@ case class OTreeIndex(index: TahoeLogFileIndex)
   def sample(
       fromPrecision: Weight,
       toPrecision: Weight,
-      files: Seq[AddFile],
-      dataFilters: Seq[Expression]): Seq[AddFile] = {
+      originalFrom: Point,
+      originalTo: Point,
+      files: Seq[AddFile]): Seq[AddFile] = {
 
     val samplingRange = RangeValues(fromPrecision, toPrecision) match {
       case range if range.to == Weight.MinValue => return List()
       case range if range.from > range.to => return List()
       case range => range
     }
-
-    val (originalFrom, originalTo) = extractRangeFilters(dataFilters, qbeastSnapshot.indexedCols)
 
     val filesVector = files.toVector
     qbeastSnapshot.spaceRevisionsMap.values
---
+++ src/main/scala/io/qbeast/spark/sql/utils/QbeastExpressionUtils.scala
@@ -0,0 +1,134 @@
+package io.qbeast.spark.sql.utils
+
+import io.qbeast.spark.index.Weight
+import io.qbeast.spark.model.Point
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.types._
+
+object QbeastExpressionUtils {
+
+  private val spark = SparkSession.active
+
+  private def hasQbeastColumnReference(
+      expr: Expression,
+      dimensionColumns: Seq[String]): Boolean = {
+    val nameEquality = spark.sessionState.analyzer.resolver
+    expr.references.forall { r =>
+      dimensionColumns.exists(nameEquality(r.name, _))
+    }
+  }
+
+  private def isQbeastExpression(expression: Expression, dimensionColumns: Seq[String]): Boolean =
+    expression.children.head.prettyName
+      .equals("qbeast_hash") || hasQbeastColumnReference(expression, dimensionColumns)
+
+  /**
+   * Analyzes the data filters from the query
+   * @param dataFilters filters passed to the relation
+   * @return min max weight (default Integer.MINVALUE, Integer.MAXVALUE)
+   */
+  def extractDataFilters(
+      dataFilters: Seq[Expression],
+      dimensionColumns: Seq[String]): (Seq[Expression], Seq[Expression]) = {
+    dataFilters.partition(expression =>
+      isQbeastExpression(expression, dimensionColumns) && !SubqueryExpression.hasSubquery(
+        expression))
+  }
+
+  /**
+   * Split conjuntive predicates from an Expression into different values for the sequence
+   * @param condition the Expression to analyze
+   * @return the sequence of all predicates
+   */
+
+  private def splitConjunctivePredicates(condition: Expression): Seq[Expression] = {
+    condition match {
+      case And(cond1, cond2) =>
+        splitConjunctivePredicates(cond1) ++ splitConjunctivePredicates(cond2)
+      case other => other :: Nil
+    }
+  }
+
+  private def extractIntValue(v: Any, dataType: DataType): Int = {
+    dataType match {
+      case ShortType => v.asInstanceOf[Short]
+      case IntegerType | DateType => v.asInstanceOf[Int]
+      case LongType | TimestampType => v.asInstanceOf[Long].toInt
+      case FloatType => v.asInstanceOf[Float].toInt
+      case DoubleType => v.asInstanceOf[Double].toInt
+      case _: DecimalType => v.asInstanceOf[Decimal].toInt
+    }
+  }
+
+  private def hasColumnReference(expr: Expression, columnName: String): Boolean = {
+    val nameEquality = spark.sessionState.analyzer.resolver
+    expr.references.forall(r => nameEquality(r.name, columnName))
+  }
+
+  /**
+   * Extracts the space range of the query
+   * @param dataFilters the filters passed by the user
+   * @param dimensionColumns the columns indexed with Qbeast
+   * @return
+   */
+
+  def extractQueryRange(
+      dataFilters: Seq[Expression],
+      dimensionColumns: Seq[String]): (Point, Point) = {
+
+    // Split conjunctive predicates
+    val filters = dataFilters.flatMap(filter => splitConjunctivePredicates(filter))
+
+    val fromTo = dimensionColumns.map(columnName => {
+
+      val columnFilters = filters.filter(hasColumnReference(_, columnName))
+
+      val from = columnFilters
+        .collectFirst {
+          case GreaterThanOrEqual(_, Literal(value, dataType)) =>
+            extractIntValue(value, dataType)
+          case EqualTo(_, Literal(value, dataType)) =>
+            extractIntValue(value, dataType)
+        }
+        .getOrElse(Int.MinValue)
+        .doubleValue()
+
+      val to = columnFilters
+        .collectFirst { case LessThan(_, Literal(value, dataType)) =>
+          extractIntValue(value, dataType)
+        }
+        .getOrElse(Int.MaxValue)
+        .doubleValue()
+
+      (from, to)
+
+    })
+
+    val from = fromTo.map(_._1)
+    val to = fromTo.map(_._2)
+
+    (Point(from.toVector), Point(to.toVector))
+  }
+
+  def extractWeightRange(filters: Seq[Expression]): (Weight, Weight) = {
+    val weightFilters = filters.filter(expression =>
+      expression.children.head.prettyName
+        .equals("qbeast_hash"))
+
+    val min = weightFilters
+      .collectFirst { case GreaterThanOrEqual(_, Literal(m, IntegerType)) =>
+        m.asInstanceOf[Int]
+      }
+      .getOrElse(Int.MinValue)
+
+    val max = weightFilters
+      .collectFirst { case LessThan(_, Literal(m, IntegerType)) =>
+        m.asInstanceOf[Int]
+      }
+      .getOrElse(Int.MaxValue)
+
+    (Weight(min), Weight(max))
+  }
+
+}
