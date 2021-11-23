63dae14f65438cda41772f16e898b164e7f39d16
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2021-11-23 17:50:12 +0100

    Changing range to WeightRange

--- core/src/main/scala/io/qbeast/model/WeightRange.scala
+++ core/src/main/scala/io/qbeast/model/WeightRange.scala
@@ -4,6 +4,5 @@ package io.qbeast.model
  * A range of values.
  * @param from the lower bound of the range
  * @param to the upper bound of the range
- * @tparam T the type of the range
  */
-case class Range[T](from: T, to: T)
+case class WeightRange(from: Weight, to: Weight)
