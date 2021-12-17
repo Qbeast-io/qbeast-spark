ea9a3b4bb47e12e997b9ecd3491638e5aa09c916
Eric Ávila <eric@qbeast.io>
2021-12-17 11:01:13 +0100

    Unify naming of variables for GroupCubeSize estimation

--- src/main/scala/io/qbeast/spark/index/OTreeDataAnalyzer.scala
+++ src/main/scala/io/qbeast/spark/index/OTreeDataAnalyzer.scala
@@ -110,12 +110,23 @@ object DoublePassOTreeDataAnalyzer extends OTreeDataAnalyzer with Serializable {
         }
     }
 
+  /**
+   * Estimates the groupCubeSize depending on the input parameters.
+   * The formula to compute the estimated value is the following:
+   *   numGroups = MAX(numPartitions, (numElements / cubeWeightsBufferCapacity))
+   *   groupCubeSize = desiredCubeSize / numGroups
+   * @param desiredCubeSize the desired cube size
+   * @param numPartitions the number of partitions
+   * @param numElements the total number of elements in the input data
+   * @param cubeWeightsBufferCapacity buffer capacity; number of elements that fit in memory
+   * @return the estimated groupCubeSize as a Double.
+   */
   private[index] def estimateGroupCubeSize(
       desiredCubeSize: Int,
       numPartitions: Int,
       numElements: Long,
-      maxWorkingSetSize: Long): Double = {
-    val numGroups = Math.max(numPartitions, numElements / maxWorkingSetSize)
+      cubeWeightsBufferCapacity: Long): Double = {
+    val numGroups = Math.max(numPartitions, numElements / cubeWeightsBufferCapacity)
     if (numGroups > 0) {
       val groupCubeSize =
         Math.ceil(desiredCubeSize.toDouble / numGroups + 1)
@@ -159,7 +170,7 @@ object DoublePassOTreeDataAnalyzer extends OTreeDataAnalyzer with Serializable {
           desiredCubeSize = desiredCubeSize,
           numPartitions = numPartitions,
           numElements = stats.filter(_.get(0) == "count").head.getAs[String](1).toLong,
-          maxWorkingSetSize =
+          cubeWeightsBufferCapacity =
             QbeastContext.config.getLong("qbeast.index.cubeWeightsBufferCapacity"))
 
       val selected = weightedDataFrame
--- src/test/scala/io/qbeast/spark/index/EstimateGroupCubeSizeTest.scala
+++ src/test/scala/io/qbeast/spark/index/EstimateGroupCubeSizeTest.scala
@@ -6,21 +6,21 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class EstimateGroupCubeSizeTest extends AnyWordSpec with Matchers with AppendedClues {
 
-  // numGroups = MAX(numPartitions, (numElements / maxWorkingSetSize))
+  // numGroups = MAX(numPartitions, (numElements / cubeWeightsBufferCapacity))
   // groupCubeSize = desiredCubeSize / numGroups
   "EstimateGroupCubeSize" when {
-    "numElements < desiredCubeSize and numElements < maxWorkingSetSize" should {
+    "numElements < desiredCubeSize and numElements < cubeWeightsBufferCapacity" should {
       "create the correct number of groups" in {
         DoublePassOTreeDataAnalyzer
           .estimateGroupCubeSize(
             desiredCubeSize = 1000000,
             numPartitions = 1,
             numElements = 10000,
-            maxWorkingSetSize = 100000) shouldBe 1000000.0 +- 1
+            cubeWeightsBufferCapacity = 100000) shouldBe 1000000.0 +- 1
       }
     }
 
-    "numElements == desiredCubeSize and numElements > maxWorkingSetSize" should {
+    "numElements == desiredCubeSize and numElements > cubeWeightsBufferCapacity" should {
       "create the correct number of groups for different number of partitions" in {
         val numPartitions = Seq(1, 10, 20)
         val groupCubeSizes = Seq(100000.0, 100000.0, 50000.0)
@@ -31,19 +31,19 @@ class EstimateGroupCubeSizeTest extends AnyWordSpec with Matchers with AppendedC
               desiredCubeSize = 1000000,
               numPartitions = nP,
               numElements = 1000000,
-              maxWorkingSetSize =
+              cubeWeightsBufferCapacity =
                 100000) shouldBe gS +- 1 withClue ("Number of partitions: " + nP)
         }
       }
     }
 
-    "numElements > desiredCubeSize and numElements > maxWorkingSetSize" should {
+    "numElements > desiredCubeSize and numElements > cubeWeightsBufferCapacity" should {
       "create the correct number of groups" in {
         DoublePassOTreeDataAnalyzer.estimateGroupCubeSize(
           desiredCubeSize = 1000000,
           numPartitions = 15,
           numElements = 2000000,
-          maxWorkingSetSize = 100000) shouldBe 50000.0 +- 1
+          cubeWeightsBufferCapacity = 100000) shouldBe 50000.0 +- 1
       }
     }
   }
