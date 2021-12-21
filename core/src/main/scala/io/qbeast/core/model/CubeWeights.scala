4026ac6b3346ee2951fbeb54e0c53e45a26b2067
osopardo1 <paolapardoat@gmail.com>
2021-12-21 14:44:37 +0100

    Revert to announcedOrReplicated set

--- core/src/main/scala/io/qbeast/core/model/CubeWeights.scala
+++ core/src/main/scala/io/qbeast/core/model/CubeWeights.scala
@@ -36,15 +36,13 @@ object CubeWeightsBuilder {
  * @param desiredCubeSize           the desired cube size
  * @param groupSize                 the boost size
  * @param bufferCapacity the buffer capacity to store the cube weights in memory
- * @param announcedSet              the announced cube identifiers
- * @param replicatedSet             the replicated cube identifiers
+ * @param replicatedOrAnnouncedSet the announced or replicated cubes' identifiers
  */
 class CubeWeightsBuilder protected (
     private val desiredCubeSize: Int,
     private val groupSize: Double,
     private val bufferCapacity: Long,
-    private val announcedSet: Set[CubeId],
-    private val replicatedSet: Set[CubeId])
+    private val replicatedOrAnnouncedSet: Set[CubeId] = Set.empty)
     extends Serializable {
 
   def this(
@@ -60,8 +58,7 @@ class CubeWeightsBuilder protected (
         numElements,
         bufferCapacity),
       bufferCapacity,
-      indexStatus.announcedSet,
-      indexStatus.replicatedSet)
+      indexStatus.replicatedOrAnnouncedSet)
 
   private val byWeight = Ordering.by[PointWeightAndParent, Weight](_.weight).reverse
   protected val queue = new mutable.PriorityQueue[PointWeightAndParent]()(byWeight)
@@ -111,7 +108,7 @@ class CubeWeightsBuilder protected (
           if (weightAndCount.count >= desiredCubeSize || weightAndCount.count == groupSize) {
             weightAndCount.weight = weight
           }
-          continue = announcedSet.contains(cubeId) || replicatedSet.contains(cubeId)
+          continue = replicatedOrAnnouncedSet.contains(cubeId)
         }
       }
     }
