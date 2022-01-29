c4a6a64a6acd3ee18b3d38ba30e5609fd7f5b7b7
Cesare Cugnasco <cesare.cugnasco@gmail.com>
2022-01-29 17:41:23 +0100

    fixing tests so we can run them individually.

--- src/test/scala/io/qbeast/spark/index/OTreeAlgorithmTest.scala
+++ src/test/scala/io/qbeast/spark/index/OTreeAlgorithmTest.scala
@@ -13,28 +13,26 @@ import org.apache.spark.sql.functions._
 class OTreeAlgorithmTest extends QbeastIntegrationTestSpec {
 
   "addRandomWeight" should
-    "be deterministic when a row have only nullable columns" in withQbeastContext() {
-      withSpark { spark =>
+    "be deterministic when a row have only nullable columns" in withQbeastAndSparkContext() {
+      spark =>
         val rdd = spark.sparkContext.parallelize(
           0.to(1000).map(i => Client1(Some(i * i), Some(s"student-$i"), Some(i))))
         val df = spark.createDataFrame(rdd)
         checkRDD(df)
-      }
+
     }
 
-  it should "be deterministic when a row have only strings" in withQbeastContext(
-  ) {
-    withSpark { spark =>
+  it should "be deterministic when a row have only strings" in
+    withQbeastAndSparkContext() { spark =>
       val rdd = spark.sparkContext.parallelize(
         0.to(1000).map(i => ClientString((i * i).toString, s"student-$i", i.toString)))
       val df = spark.createDataFrame(rdd)
       checkRDD(df)
-    }
-  }
 
-  it should "be deterministic when a row have only optional strings" in withQbeastContext(
-  ) {
-    withSpark { spark =>
+    }
+
+  it should "be deterministic when a row have only optional strings" in
+    withQbeastAndSparkContext() { spark =>
       val rdd = spark.sparkContext.parallelize(
         0.to(1000)
           .map(i =>
@@ -42,50 +40,44 @@ class OTreeAlgorithmTest extends QbeastIntegrationTestSpec {
       val df = spark.createDataFrame(rdd)
       checkRDD(df)
     }
-  }
 
-  it should "be deterministic with real data" in withQbeastContext() {
-    withSpark { spark =>
-      val inputPath = "src/test/resources/"
-      val file1 = "ecommerce300k_2019_Nov.csv"
-      val df = spark.read
-        .format("csv")
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .load(inputPath + file1)
-        .distinct()
-      checkRDD(df)
-    }
+  it should "be deterministic with real data" in withQbeastAndSparkContext() { spark =>
+    val inputPath = "src/test/resources/"
+    val file1 = "ecommerce300k_2019_Nov.csv"
+    val df = spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load(inputPath + file1)
+      .distinct()
+    checkRDD(df)
   }
 
   it should "be deterministic when a row has only nullable columns" +
-    " and null values" in withQbeastContext() {
-      withSpark { spark =>
-        val rdd = spark.sparkContext.parallelize(0.to(1000).map {
-          case i if i % 3 == 0 => Client1(Some(i * i), None, Some(i))
-          case i if i % 5 == 0 => Client1(Some(i * i), Some(s"student-$i"), None)
-          case i if i % 7 == 0 => Client1(None, Some(s"student-$i"), Some(i))
-          case i if i % 11 == 0 => Client1(None, None, None)
-          case i => Client1(Some(i * i), Some(s"student-$i"), Some(i))
-        })
-        val df = spark.createDataFrame(rdd)
-        checkRDD(df)
-      }
+    " and null values" in withQbeastAndSparkContext() { spark =>
+      val rdd = spark.sparkContext.parallelize(0.to(1000).map {
+        case i if i % 3 == 0 => Client1(Some(i * i), None, Some(i))
+        case i if i % 5 == 0 => Client1(Some(i * i), Some(s"student-$i"), None)
+        case i if i % 7 == 0 => Client1(None, Some(s"student-$i"), Some(i))
+        case i if i % 11 == 0 => Client1(None, None, None)
+        case i => Client1(Some(i * i), Some(s"student-$i"), Some(i))
+      })
+      val df = spark.createDataFrame(rdd)
+      checkRDD(df)
+
     }
 
-  it should "be deterministic when a row have only NOT nullable columns" in withQbeastContext(
-  ) {
-    withSpark { spark =>
+  it should "be deterministic when a row have only NOT nullable columns" in
+    withQbeastAndSparkContext() { spark =>
       val rdd =
         spark.sparkContext.parallelize(0.to(1000).map(i => Client2(i * i, s"student-$i", i)))
       val df = spark.createDataFrame(rdd)
       checkRDD(df)
 
     }
-  }
 
   def checkRDD(df: DataFrame): Unit =
-    withQbeastContext() {
+    withQbeastAndSparkContext() { _ =>
       val rev = SparkRevisionFactory.createNewRevision(
         QTableID("test"),
         df.schema,
