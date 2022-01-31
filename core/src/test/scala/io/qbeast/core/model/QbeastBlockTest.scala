eeb56ad09b913c0ddea0088fcf76c832d89d66ed
osopardo1 <paolapardoat@gmail.com>
2022-01-31 10:54:32 +0100

    Fix QbeastBlockTest compile error

--- core/src/test/scala/io/qbeast/core/model/QbeastBlockTest.scala
+++ core/src/test/scala/io/qbeast/core/model/QbeastBlockTest.scala
@@ -5,37 +5,34 @@ import org.scalatest.matchers.should.Matchers
 
 class QbeastBlockTest extends AnyFlatSpec with Matchers {
   "QbeastBlock" should "find all the keys in the map" in {
-    val fileMetadata: Map[String, String] = Map(
-      "cube" -> CubeId.root(1).string,
+    val blockMetadata: Map[String, String] = Map(
       "minWeight" -> "19217",
       "maxWeight" -> "11111111",
       "state" -> "FlOODED",
       "revision" -> "1",
       "elementCount" -> "777")
 
-    val qbeastFile = QbeastBlock("path", fileMetadata, 0L, 0L)
-    qbeastFile.cube shouldBe CubeId.root(1).string
-    qbeastFile.minWeight shouldBe Weight(19217)
-    qbeastFile.maxWeight shouldBe Weight(11111111)
-    qbeastFile.state shouldBe "FlOODED"
-    qbeastFile.revision shouldBe 1
-    qbeastFile.elementCount shouldBe 777
+    val qbeastBlock = QbeastBlock("path", blockMetadata, 0L, 0L)
+    qbeastBlock.minWeight shouldBe Weight(19217)
+    qbeastBlock.maxWeight shouldBe Weight(11111111)
+    qbeastBlock.state shouldBe "FlOODED"
+    qbeastBlock.revision shouldBe 1
+    qbeastBlock.elementCount shouldBe 777
   }
 
   it should "throw exception if key not found" in {
-    val fileMetadata = Map.empty[String, String]
-    a[IllegalArgumentException] shouldBe thrownBy(QbeastBlock("path", fileMetadata, 0L, 0L))
+    val blockMetadata = Map.empty[String, String]
+    a[IllegalArgumentException] shouldBe thrownBy(QbeastBlock("path", blockMetadata, 0L, 0L))
   }
 
   it should "throw error if the types are different" in {
-    val fileMetadata: Map[String, String] = Map(
-      "cube" -> CubeId.root(1).string,
+    val blockMetadata: Map[String, String] = Map(
       "minWeight" -> "19217",
       "maxWeight" -> "11111111",
       "state" -> "FlOODED",
       "revision" -> "bad_type",
       "elementCount" -> "777")
 
-    a[IllegalArgumentException] shouldBe thrownBy(QbeastBlock("path", fileMetadata, 0L, 0L))
+    a[IllegalArgumentException] shouldBe thrownBy(QbeastBlock("path", blockMetadata, 0L, 0L))
   }
 }
