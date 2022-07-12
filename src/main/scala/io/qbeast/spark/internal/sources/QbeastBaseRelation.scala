eecb6fab4ed24e4c9aa701ed6307ec216e113f8c
Adrià Correas <adriacorgri@gmail.com>
2022-07-12 16:52:47 +0200

    code snippet in QbeastDataSource, fileFormat missing

--- src/main/scala/io/qbeast/spark/internal/sources/QbeastBaseRelation.scala
+++ src/main/scala/io/qbeast/spark/internal/sources/QbeastBaseRelation.scala
@@ -7,9 +7,29 @@ import io.qbeast.IISeq
 import io.qbeast.core.model.{QTableID, Revision}
 import io.qbeast.spark.delta.SparkDeltaMetadataManager
 import io.qbeast.core.transform.Transformer
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{SQLContext, DataFrame, AnalysisExceptionFactory}
 import org.apache.spark.sql.sources.BaseRelation
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{StructType, StructField}
+import org.apache.spark.sql.sources.InsertableRelation
+import org.apache.spark.sql.SparkSession
+import io.qbeast.spark.delta.OTreeIndex
+import org.apache.spark.sql.execution.datasources.HadoopFsRelation
+import org.apache.spark.sql.delta.files.TahoeLogFileIndex
+
+import org.apache.spark.sql.delta.DeltaFileFormat
+import io.qbeast.spark.table.IndexedTableFactory
+import io.qbeast.spark.utils.SparkToQTypesUtils
+import org.apache.spark.sql.execution.datasources.FileFormat
+
+import org.apache.hadoop.fs.{Path}
+import org.apache.spark.sql.catalyst.catalog.{BucketSpec}
+import org.apache.spark.sql.{
+  AnalysisExceptionFactory,
+  DataFrame,
+  SQLContext,
+  SaveMode,
+  SparkSession
+}
 
 /**
  * Implementation of BaseRelation which wraps the Delta relation.
@@ -18,10 +38,16 @@ import org.apache.spark.sql.types.StructType
  * @param revision the revision to use
  */
 case class QbeastBaseRelation(relation: BaseRelation, private val revision: Revision)
-    extends BaseRelation {
+    extends BaseRelation
+    with InsertableRelation
+    with DeltaFileFormat {
   override def sqlContext: SQLContext = relation.sqlContext
 
   override def schema: StructType = relation.schema
+
+  // override def insert(data: DataFrame, overwrite: Boolean): Unit = {
+  //   asSelectQuery = Option(data)
+  // }
 
   def columnTransformers: IISeq[Transformer] = revision.columnTransformers
 
@@ -39,8 +65,11 @@ object QbeastBaseRelation {
    */
 
   def forDeltaTable(tableID: QTableID): QbeastBaseRelation = {
+
     val log = SparkDeltaMetadataManager.loadDeltaQbeastLog(tableID)
+
     QbeastBaseRelation(log.createRelation, log.qbeastSnapshot.loadLatestRevision)
+
   }
 
 }
