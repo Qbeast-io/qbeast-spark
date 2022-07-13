c4854dd5cd975cea2131940ddaf605f846364548
osopardo1 <paolapardoat@gmail.com>
2022-07-13 17:31:49 +0200

    Change RunnableCommand for LeafRunnableCommand

--- src/main/scala/io/qbeast/spark/internal/commands/CompactTableCommand.scala
+++ src/main/scala/io/qbeast/spark/internal/commands/CompactTableCommand.scala
@@ -6,19 +6,14 @@ package io.qbeast.spark.internal.commands
 import io.qbeast.core.model.RevisionID
 import io.qbeast.spark.table.IndexedTable
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.execution.command.RunnableCommand
+import org.apache.spark.sql.execution.command.LeafRunnableCommand
 
 case class CompactTableCommand(revisionID: RevisionID, indexedTable: IndexedTable)
-    extends RunnableCommand {
+    extends LeafRunnableCommand {
 
   override def run(sparkSession: SparkSession): Seq[Row] = {
     indexedTable.compact(revisionID)
     Seq.empty
   }
-
-  override protected def withNewChildrenInternal(
-      newChildren: IndexedSeq[LogicalPlan]): LogicalPlan =
-    SparkSession.active.emptyDataFrame.queryExecution.logical
 
 }
