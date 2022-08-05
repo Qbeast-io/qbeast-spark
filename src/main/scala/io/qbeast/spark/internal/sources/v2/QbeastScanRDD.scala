f6188fff39d5727c21580d6f311fceed471ba4bc
osopardo1 <paolapardoat@gmail.com>
2022-08-05 09:32:15 +0200

    Extend DeltaCatalog and modify buildScan() method

--- src/main/scala/io/qbeast/spark/internal/sources/v2/QbeastScanRDD.scala
+++ src/main/scala/io/qbeast/spark/internal/sources/v2/QbeastScanRDD.scala
@@ -7,15 +7,12 @@ import io.qbeast.context.QbeastContext._
 import io.qbeast.spark.internal.sources.QbeastBaseRelation
 import io.qbeast.spark.table.IndexedTable
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.catalyst.encoders.{RowEncoder}
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.connector.read.V1Scan
-import org.apache.spark.sql.execution.FileSourceScanExec
-import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelation}
+import org.apache.spark.sql.execution.datasources.HadoopFsRelation
 import org.apache.spark.sql.sources.{BaseRelation, TableScan}
 import org.apache.spark.sql.types.StructType
-
-import scala.collection.JavaConverters.asScalaIteratorConverter
 
 /**
  * Extends Scan Builder for V1 DataSource
@@ -37,37 +34,31 @@ class QbeastScanRDD(indexedTable: IndexedTable) extends V1Scan {
       override def schema: StructType = qbeastBaseRelation.schema
 
       override def buildScan(): RDD[Row] = {
-        val schema = qbeastBaseRelation.schema
 
-        // Ugly hack to convert the schema fields into Attributes
-        val output = LogicalRelation
-          .apply(qbeastBaseRelation)
-          .resolve(schema, context.sparkSession.sessionState.analyzer.resolver)
+        val rootPath = indexedTable.tableID.id
 
-        // TODO check the parameters passed to this method
-        FileSourceScanExec(
-          relation = qbeastBaseRelation,
-          output = output,
-          requiredSchema = schema,
-          partitionFilters = Seq.empty,
-          optionalBucketSet = None,
-          optionalNumCoalescedBuckets = None,
-          dataFilters = Seq.empty,
-          tableIdentifier = None)
-          .executeColumnar()
-          .mapPartitions { batches =>
-            batches.flatMap { batch =>
-              batch
-                .rowIterator()
-                .asScala
-                .map(internal => {
-                  val encoder = RowEncoder(schema).resolveAndBind()
-                  encoder.createDeserializer().apply(internal)
-                })
-            }
-          }
+        // Get the files to load from the Relation index
+        val filesToLoad = qbeastBaseRelation.location.listFiles(Seq.empty, Seq.empty)
+        // Map the paths of the files with the rootPath
+        val pathsToLoad =
+          filesToLoad.flatMap(_.files.map(f => {
+            val path = f.getPath
+            if (path.isAbsolute) path.toString else rootPath + "/" + path.toString
+          }))
+
+        // We output the plan to build scan information
+        // This is a hack, and the Scan should be done
+        // with more Qbeast logic
+        val df = context.sparkSession.read.format("parquet").load(pathsToLoad: _*)
+        val encoder = RowEncoder(schema).resolveAndBind()
+        val deserializer = encoder.createDeserializer()
+
+        df.queryExecution.executedPlan.execute().mapPartitions { batches =>
+          batches.map(deserializer.apply)
+        }
       }
     }.asInstanceOf[T]
+
   }
 
   override def readSchema(): StructType = metadataManager.loadCurrentSchema(indexedTable.tableID)
