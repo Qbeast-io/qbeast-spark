5d4e0bca36bbb39b552e45705ac25abc978484ee
Jiawei <jiaweihu08@gmail.com>
2022-09-09 17:55:12 +0200

    Access deltaLog/snapshot and modify log

--- src/main/scala/io/qbeast/spark/internal/commands/ConvertToQbeastCommand.scala
+++ src/main/scala/io/qbeast/spark/internal/commands/ConvertToQbeastCommand.scala
@@ -3,10 +3,16 @@
  */
 package io.qbeast.spark.internal.commands
 
+import io.qbeast.core.model.{BroadcastedTableChanges, CubeId, QTableID, Weight}
+import io.qbeast.spark.delta.SparkDeltaMetadataManager
+import io.qbeast.spark.index.SparkRevisionFactory
+import io.qbeast.spark.utils.{State, TagUtils}
 import org.apache.http.annotation.Experimental
 import org.apache.spark.qbeast.config.DEFAULT_CUBE_SIZE
+import org.apache.spark.sql.delta.DeltaLog
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.execution.command.{LeafRunnableCommand}
+import org.apache.spark.sql.execution.command.LeafRunnableCommand
+import scala.util.matching.Regex
 
 @Experimental
 case class ConvertToQbeastCommand(
@@ -16,21 +22,66 @@ case class ConvertToQbeastCommand(
     cubeSize: Int = DEFAULT_CUBE_SIZE)
     extends LeafRunnableCommand {
 
-  private val acceptedFormats = Seq("parquet", "delta")
+  private val parquetFormat = "parquet"
+  private val deltaFormat = "delta"
+  private val acceptedFormats = Seq(parquetFormat, deltaFormat)
+  private val numRecordsPattern: Regex = """"numRecords":(\d+),""".r
+
+  def convertParquetToDelta(path: String): Unit = {}
 
   override def run(sparkSession: SparkSession): Seq[Row] = {
     // TODO very basic mechanism for converting to qbeast
+    assert(acceptedFormats.contains(fileFormat), s"Format $fileFormat not supported.")
+
+    if (fileFormat == parquetFormat) convertParquetToDelta(path)
+
     val options =
       Map("columnsToIndex" -> columnsToIndex.mkString(","), "cubeSize" -> cubeSize.toString)
-    if (acceptedFormats.contains(fileFormat)) {
-      val df = sparkSession.read.format(fileFormat).load(path)
-      df.write.format("qbeast").mode("overwrite").options(options).save(path)
-    } else {
-      throw new UnsupportedOperationException(s"Unsupported file format: $fileFormat")
+
+    val deltaLog = DeltaLog.forTable(sparkSession, path)
+    val snapshot = deltaLog.snapshot
+    val deltaFiles = snapshot.allFiles.collect()
+
+    val revision =
+      SparkRevisionFactory.createNewRevision(QTableID(path), snapshot.schema, options)
+
+    // Assume all files are AddFiles
+    val newFiles = deltaFiles
+      .map(file => {
+        val elementCount = numRecordsPattern.findFirstMatchIn(file.stats) match {
+          case Some(matching) => matching.group(1)
+          case _ => "0"
+        }
+        println(elementCount)
+        file.copy(tags = Map(
+          TagUtils.cube -> "",
+          TagUtils.minWeight -> Weight.MinValue.value.toString,
+          TagUtils.maxWeight -> Weight.MaxValue.value.toString,
+          TagUtils.state -> State.FLOODED,
+          TagUtils.revision -> revision.revisionID.toString,
+          TagUtils.elementCount -> elementCount))
+      })
+      .toIndexedSeq
+
+    SparkDeltaMetadataManager.updateWithTransaction(
+      revision.tableID,
+      snapshot.schema,
+      append = false) {
+      val tableChanges = BroadcastedTableChanges(
+        isNewRevision = false,
+        isOptimizeOperation = false,
+        revision,
+        Set.empty[CubeId],
+        Set.empty[CubeId],
+        SparkSession.active.sparkContext.broadcast(
+          Map(revision.createCubeIdRoot() -> State.FLOODED)),
+        SparkSession.active.sparkContext.broadcast(
+          Map(revision.createCubeIdRoot -> Weight.MaxValue)))
+
+      (tableChanges, newFiles)
     }
-
+//      throw new UnsupportedOperationException(s"Unsupported file format: $fileFormat")
     Seq.empty
-
   }
 
 }
