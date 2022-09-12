9a2236bef470415bd09c87e30eaab7f3c525efe3
Jiawei <jiaweihu08@gmail.com>
2022-09-12 14:07:25 +0200

    Converted tables should be readable using both formates

--- src/test/scala/io/qbeast/spark/utils/ConvertToQbeastTest.scala
+++ src/test/scala/io/qbeast/spark/utils/ConvertToQbeastTest.scala
@@ -2,9 +2,26 @@ package io.qbeast.spark.utils
 
 import io.qbeast.spark.QbeastIntegrationTestSpec
 import io.qbeast.spark.internal.commands.ConvertToQbeastCommand
-import org.apache.spark.sql.AnalysisException
+import org.apache.spark.sql.{AnalysisException, SparkSession}
+import org.apache.spark.sql.delta.DeltaLog
 
 class ConvertToQbeastTest extends QbeastIntegrationTestSpec {
+
+//  def addCubeTag: UserDefinedFunction = {
+//    udf((elementCount: String) =>
+//      Map(
+//        "state" -> "FLOODED",
+//        "cube" -> "",
+//        "revision" -> 0,
+//        "minWeight" -> Int.MinValue.toString,
+//        "maxWeight" -> Int.MaxValue.toString,
+//        "elementCount" -> elementCount))
+//  }
+//
+  def showFileLog(spark: SparkSession, path: String): Unit = {
+    val snapshot = DeltaLog.forTable(spark, path).snapshot
+    snapshot.allFiles.show(false)
+  }
 
   "ConvertToQbeast" should "convert a Parquet Table to a Qbeast Table" in withSparkAndTmpDir(
     (spark, tmpDir) => {
@@ -59,4 +76,33 @@ class ConvertToQbeastTest extends QbeastIntegrationTestSpec {
           .run(spark))
 
     })
+
+//  "A converted delta table" should "be readable using delta" in withSparkAndTmpDir(
+//    (spark, tmpDir) => {
+//      val df = loadTestData(spark)
+//      df.write.format("delta").save(tmpDir)
+//      showFileLog(spark, tmpDir)
+//
+//      val fileFormat = "delta"
+//      val columnsToIndex = Seq("user_id", "price", "category_id")
+//      ConvertToQbeastCommand(tmpDir, fileFormat, columnsToIndex, 500000).run(spark)
+//      showFileLog(spark, tmpDir)
+//
+//      spark.read.format("delta").load(tmpDir).count shouldBe df.count
+//    })
+//
+//  "A converted delta table" should "be readable using qbeast" in withSparkAndTmpDir(
+//    (spark, tmpDir) => {
+//      val df = loadTestData(spark)
+//      df.write.format("delta").save(tmpDir)
+//      showFileLog(spark, tmpDir)
+//
+//      val fileFormat = "delta"
+//      val columnsToIndex = Seq("user_id", "price", "category_id")
+//      ConvertToQbeastCommand(tmpDir, fileFormat, columnsToIndex, 500000).run(spark)
+//      showFileLog(spark, tmpDir)
+//
+//      spark.read.format("qbeast").load(tmpDir).count shouldBe df.count
+//    })
+
 }
