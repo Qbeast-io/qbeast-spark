471cb06a7ac93e73cc5d7a060e0d7bd3f86309f3
Jiawei <jiaweihu08@gmail.com>
2022-09-20 18:07:47 +0200

    Reformat tests

--- src/test/scala/io/qbeast/spark/utils/ConvertToQbeastTest.scala
+++ src/test/scala/io/qbeast/spark/utils/ConvertToQbeastTest.scala
@@ -2,29 +2,39 @@ package io.qbeast.spark.utils
 
 import io.qbeast.spark.QbeastIntegrationTestSpec
 import io.qbeast.spark.internal.commands.ConvertToQbeastCommand
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.delta.DeltaLog
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class ConvertToQbeastTest extends QbeastIntegrationTestSpec {
   val columnsToIndex: Seq[String] = Seq("user_id", "price", "category_id")
+  val dataSize = 99986 // loadTestData(spark).count
 
-  def showFileLog(spark: SparkSession, path: String, truncate: Boolean = true): Unit = {
-    // scalastyle:off println
-    val snapshot = DeltaLog.forTable(spark, path).snapshot
+  def convertFormatsFromTo(
+      sourceFormat: String,
+      targetFormat: String,
+      spark: SparkSession,
+      dir: String,
+      columnsToIndex: Seq[String] = columnsToIndex): DataFrame = {
+    val data = loadTestData(spark)
+    data.write.mode("overwrite").format(sourceFormat).save(dir)
 
-    println("AddFiles:")
-    snapshot.allFiles.show(truncate)
+    ConvertToQbeastCommand(dir, sourceFormat, columnsToIndex).run(spark)
 
-    println("RemoveFiles:")
-    snapshot.tombstones.show(truncate)
+    spark.read.format(targetFormat).load(dir)
   }
 
-  "ConvertToQbeast" should "convert a Parquet Table to a Qbeast Table" in withSparkAndTmpDir(
-    (spark, tmpDir) => {})
+  "ConvertToQbeastCommand" should "convert a Delta Table into a Qbeast Table" in withSparkAndTmpDir(
+    (spark, tmpDir) => {
+      val qDf = convertFormatsFromTo("delta", "qbeast", spark, tmpDir)
+      qDf.count shouldBe dataSize
+    })
 
-  it should "convert a Delta Table to a Qbeast Table" in withSparkAndTmpDir((spark, tmpDir) => {})
+  it should "convert a Parquet Table into a Qbeast Table" in
+    withSparkAndTmpDir((spark, tmpDir) => {
+      val qDf = convertFormatsFromTo("parquet", "qbeast", spark, tmpDir)
+      qDf.count shouldBe dataSize
+    })
 
-  it should "throw an error when attempting to convert to unsupported format" in withSparkAndTmpDir(
+  it should "throw an error when attempting to convert an unsupported format" in withSparkAndTmpDir(
     (spark, tmpDir) => {
       val df = loadTestData(spark)
       df.write.mode("overwrite").json(tmpDir)
@@ -47,29 +57,42 @@ class ConvertToQbeastTest extends QbeastIntegrationTestSpec {
 //
 //    })
 
-  "A converted delta table" should "be readable using delta" in withSparkAndTmpDir(
+  it should "throw an error if columnsToIndex are not found in table schema" in withSparkAndTmpDir(
     (spark, tmpDir) => {
-      val df = loadTestData(spark)
-      df.write.format("delta").save(tmpDir)
-//      showFileLog(spark, tmpDir)
-
-      val fileFormat = "delta"
-      ConvertToQbeastCommand(tmpDir, fileFormat, columnsToIndex).run(spark)
-//      showFileLog(spark, tmpDir, truncate = false)
-
-      spark.read.format("delta").load(tmpDir).count shouldBe df.count
+      val nonExistentColumns = Seq("a", "b")
+      an[RuntimeException] shouldBe thrownBy(
+        convertFormatsFromTo("delta", "qbeast", spark, tmpDir, nonExistentColumns))
     })
 
-  it should "be readable using qbeast" in withSparkAndTmpDir((spark, tmpDir) => {
-    val df = loadTestData(spark)
-    val fileFormat = "delta"
-    df.write.format(fileFormat).save(tmpDir)
-//    showFileLog(spark, tmpDir)
+  it should "should be idempotent" in withSparkAndTmpDir((spark, tmpDir) => {})
 
-    ConvertToQbeastCommand(tmpDir, fileFormat, columnsToIndex).run(spark)
-//    showFileLog(spark, tmpDir, truncate = false)
+  it should "create correct OTree metrics" in withSparkAndTmpDir((spark, tmpDir) => {})
 
-    spark.read.format("qbeast").load(tmpDir).count shouldBe df.count
+  it should "allow correct execution of Analyze and Optimize" in withSparkAndTmpDir(
+    (spark, tmpDir) => {})
+
+  it should "allow correct execution of Compaction" in withSparkAndTmpDir((spark, tmpDir) => {})
+
+  "A converted delta table" should "be readable using delta" in withSparkAndTmpDir(
+    (spark, tmpDir) => {
+      val qDf = convertFormatsFromTo("delta", "delta", spark, tmpDir)
+      qDf.count shouldBe dataSize
+    })
+
+  it should "be readable using parquet" in withSparkAndTmpDir((spark, tmpDir) => {
+    val qDf = convertFormatsFromTo("delta", "parquet", spark, tmpDir)
+    qDf.count shouldBe dataSize
   })
+
+//  "A converted parquet table" should "be readable using delta" in withSparkAndTmpDir(
+//    (spark, tmpDir) => {
+//      val qDf = convertFormatsFromTo("parquet", "delta", spark, tmpDir)
+//      qDf.count shouldBe dataSize
+//    })
+//
+//  it should "be readable using parquet" in withSparkAndTmpDir((spark, tmpDir) => {
+//    val qDf = convertFormatsFromTo("parquet", "parquet", spark, tmpDir)
+//    qDf.count shouldBe dataSize
+//  })
 
 }
