ee78dddd6c7e1a45090653193a26091d35557a2a
Jiawei <jiaweihu08@gmail.com>
2022-10-06 19:30:41 +0200

    Use updated cubeSizes

--- core/src/main/scala/io/qbeast/core/model/CubeNormalizedWeights.scala
+++ core/src/main/scala/io/qbeast/core/model/CubeNormalizedWeights.scala
@@ -76,20 +76,13 @@ object CubeNormalizedWeights {
       maxDepth = maxDepth.max(cubeId.depth)
     }
 
-    // scalastyle:off println
-    println(s"Estimated initial cubeSizes:")
-    cubeSizes.result().toSeq.sortBy(_._1).foreach { case (cube, size) =>
-      val weight = mergedNormalizedCubeWeights(cube)
-      println(s"cube: $cube, size: $size, NormalizedWeight: $weight")
-    }
-
     val rollUpMap = if (maxDepth > 0) {
       accumulativeRollUp(
         cubeRollUpMap.result(),
         cubeSizes.result(),
         announcedOrReplicatedSet,
         maxDepth,
-        (desiredCubeSize * 1.2).toInt)
+        desiredCubeSize)
     } else {
       Map.empty[CubeId, CubeId]
     }
@@ -115,47 +108,49 @@ object CubeNormalizedWeights {
       announcedOrReplicatedSet: Set[CubeId],
       maxDepth: Int,
       maxRollingSize: Int): Map[CubeId, CubeId] = {
-    val levelCubeSizes = cubeSizes.groupBy(_._1.depth)
+    val cubesPerLevel = cubeSizes.keys.groupBy(_.depth)
 
     // Iterate from maxDepth to level 1, avoiding the root
     (maxDepth to 1 by -1)
-      .filter(levelCubeSizes.contains)
+      .filter(cubesPerLevel.contains)
       .foreach { depth =>
         // Cube sizes from the current level
-        levelCubeSizes(depth)
+        cubesPerLevel(depth)
           // We operate on cubes that are neither ANNOUNCED nor REPLICATED, for these have
           // replications from their ancestors, performing roll-up will corrupt OTree integrity
-          .filter { case (cube, _) => !announcedOrReplicatedSet.contains(cube) }
+          .filterNot(announcedOrReplicatedSet.contains)
           // Group cubes by their parent in order to find sibling cubes
-          .groupBy { case (cube, _) => cube.parent.get }
+          .groupBy(_.parent.get)
           // Make sure parent cube existing in the map and it's not an ANNOUNCED nor REPLICATED cube
           .filter { case (parent, _) =>
             cubeSizes.contains(parent) && !announcedOrReplicatedSet.contains(parent)
           }
           // Process sibling cubes from the current level
-          .foreach { case (parent, siblingCubeSizes) =>
+          .foreach { case (parent, siblingCubes) =>
             // Take the smallest cube each time and add them to the parent until either
             // all cubes are rolled-up or the size has grown too large
-            val sortedCubeIter = siblingCubeSizes.toSeq.sortBy(_._2).toIterator
-            var newSize = 0L
+            val sortedCubeIter =
+              siblingCubes.map(c => (c, cubeSizes(c))).toSeq.sortBy(_._2).toIterator
+            var accSize = 0L
             var continue = true
             while (continue && sortedCubeIter.hasNext) {
-              val (cube, size) = sortedCubeIter.next()
-              continue = newSize + size < maxRollingSize
-              if (continue) {
+              val (child, size) = sortedCubeIter.next()
+              if (accSize + size <= maxRollingSize) {
                 // Increment size
-                newSize += size
+                accSize += size
                 // Map child to parent
-                cubeRollUpMap(cube) = parent
+                cubeRollUpMap(child) = parent
+              } else {
+                continue = false
               }
             }
             // Update parent size
-            cubeSizes(parent) = newSize + cubeSizes(parent)
+            cubeSizes(parent) += accSize
           }
       }
 
     // Convert A -> B -> C -> D to A -> D
-    val rollUpMap = cubeRollUpMap.keys
+    cubeRollUpMap.keys
       .map(cube => {
         var targetCube: CubeId = cubeRollUpMap(cube)
         while (cubeRollUpMap(targetCube) != targetCube) {
@@ -164,18 +159,6 @@ object CubeNormalizedWeights {
         (cube, targetCube)
       })
       .toMap
-
-    // scalastyle:off println
-    println(s"Compressed cubeSizes:")
-    cubeSizes
-      .result()
-      .keys
-      .foreach(cube => {
-        val mappedCube = rollUpMap(cube)
-        println(s"cube: $cube, mappedCube: $mappedCube, size: ${cubeSizes(mappedCube)}")
-      })
-
-    rollUpMap
   }
 
 }
