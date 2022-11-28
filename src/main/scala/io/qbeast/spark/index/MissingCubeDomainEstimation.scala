bc3e8a2a10686eb843ddfc9fb01eed2c5c110191
Jiawei <jiaweihu08@gmail.com>
2022-11-28 14:48:31 +0100

    Implement fine-grained cube domain estimation for missing cubes

--- src/main/scala/io/qbeast/spark/index/MissingCubeDomainEstimation.scala
+++ src/main/scala/io/qbeast/spark/index/MissingCubeDomainEstimation.scala
@@ -23,4 +23,59 @@ object MissingCubeDomainEstimation {
     parentOption
   }
 
+  /**
+   * Given a CubeId C and its LocalTree, compute the size of its parent payload as well as its
+   * fraction that fall within the subspace of C. C and C.parent are assumed to be present in
+   * the given LocalTree.
+   */
+  private[index] def parentFractionAndPayload(
+      ancestor: CubeId,
+      localTree: LocalTree): (Double, Double) = {
+    // The fraction f from A's payload that belongs to C's branch is computed as:
+    // f = TreeSize(C) / (TreeSize(B) + TreeSize(C), TreeSize(D))
+    // LocalTree:
+    //                    A
+    //                  / | \
+    //                 B  C  D
+    val ancestorTreeSize = localTree(ancestor).treeSize
+
+    val parent = ancestor.parent.get
+    val parentTreeSize = localTree(parent).treeSize
+    val siblingTreeSize =
+      parent.children.filter(localTree.contains).map(localTree(_).treeSize).sum
+
+    val f = ancestorTreeSize / siblingTreeSize
+    val payload = parentTreeSize - siblingTreeSize
+
+    (f, payload)
+  }
+
+  /**
+   *  For each existing ancestor of the missing cube, compute the fraction of
+   *  their payload that belongs to our target domain. The payload distribution of a given cube
+   *  is assumed to be the same as that of its child cube tree sizes.
+   */
+  private[index] def domainThroughPayloadFractions(
+      missingCube: CubeId,
+      localTree: LocalTree): Double = {
+    findClosestAncestorBottomUp(localTree, missingCube) match {
+      case None => 0d
+      case Some(cube) =>
+        var ancestor = cube
+        var (payloadFraction, localDomain) = (1d, 0d)
+        // Stop the computation when the ancestor is root or,
+        // its parent is missing from the LocalTree, which happens during optimization.
+        while (!ancestor.isRoot && localTree.contains(ancestor.parent.get)) {
+          val (fraction, payload) = parentFractionAndPayload(ancestor, localTree)
+          // Update fraction using accumulative product, and update the domain.
+          payloadFraction *= fraction
+          localDomain += payload * payloadFraction
+
+          ancestor = ancestor.parent.get
+        }
+
+        localDomain
+    }
+  }
+
 }
