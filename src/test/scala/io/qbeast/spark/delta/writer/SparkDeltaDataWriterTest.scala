6e919f2964e3a38f445d2a6d8615d5e83a3a5cc1
Alexey Akimov <alexey@qbeast.io>
2023-10-09 14:07:56 +0200

    #207 SparkDeltaDataWriterTest is improved

--- src/test/scala/io/qbeast/spark/delta/writer/SparkDeltaDataWriterTest.scala
+++ src/test/scala/io/qbeast/spark/delta/writer/SparkDeltaDataWriterTest.scala
@@ -8,6 +8,10 @@ import io.qbeast.spark.index.SparkOTreeManager
 import io.qbeast.spark.index.SparkRevisionFactory
 
 import scala.reflect.io.Path
+import io.qbeast.spark.delta.DeltaQbeastSnapshot
+import org.apache.spark.sql.delta.DeltaLog
+import org.apache.spark.sql.delta.actions.RemoveFile
+import org.apache.spark.sql.delta.actions.AddFile
 
 class SparkDeltaDataWriterTest extends QbeastIntegrationTestSpec {
 
@@ -35,4 +39,28 @@ class SparkDeltaDataWriterTest extends QbeastIntegrationTestSpec {
       }
     }
 
+  it should "compact the data correctly" in withSparkAndTmpDir { (spark, tmpDir) =>
+    val data = spark.createDataFrame((0 to 10000).map(i =>
+      Client4(i * i, s"student-$i", Some(i), Some(i * 1000 + 123), Some(i * 2567.3432143))))
+    val options = Map("columnsToIndex" -> "age,val2", "cubeSize" -> "1000", "fileSize" -> "1000")
+    data.write.format("qbeast").options(options).save(tmpDir)
+
+    val files = DeltaQbeastSnapshot(
+      DeltaLog.forTable(spark, tmpDir).unsafeVolatileSnapshot).loadLatestIndexFiles
+    val smallFilePaths =
+      files
+        .filter(_.elementCount < 1000)
+        .map(_.file.path)
+        .toSet
+
+    val actions = SparkDeltaDataWriter.compact(QTableID(tmpDir), data.schema, 1, files, 1000)
+    actions
+      .filter(_.isInstanceOf[AddFile])
+      .map(_.path)
+      .foreach(smallFilePaths shouldNot contain(_))
+    actions
+      .filter(_.isInstanceOf[RemoveFile])
+      .map(_.path)
+      .foreach(smallFilePaths should contain(_))
+  }
 }
