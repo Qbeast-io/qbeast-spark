04f517e1836200e2c111eb1dce60e295e4d67854
Alexey Akimov <alexey@qbeast.io>
2023-11-22 15:48:14 +0100

    #207 Unused tags and Qbeast columns are removed

--- src/main/scala/io/qbeast/spark/utils/Params.scala
+++ src/main/scala/io/qbeast/spark/utils/Params.scala
@@ -18,21 +18,11 @@ object State {
  * Tag keys for saving qbeast index metadata into the delta commit log
  */
 object TagColumns {
-  final val cube = col("tags.cube")
-  final val minWeight = col("tags.minWeight")
-  final val maxWeight = col("tags.maxWeight")
-  final val state = col("tags.state")
   final val revision = col("tags.revision")
-  final val elementCount = col("tags.elementCount")
 }
 
 object TagUtils {
-  final val cube = "cube"
-  final val minWeight = "minWeight"
-  final val maxWeight = "maxWeight"
-  final val state = "state"
   final val revision = "revision"
-  final val elementCount = "elementCount"
   final val blocks = "blocks"
 }
 
