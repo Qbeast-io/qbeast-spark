cce723cabc56484326a3873adaa0560f09fa0b89
jiawei <jiaweihu08@gmail.com>
2024-02-26 15:19:56 +0100

    Upgrade delta lake version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object Dependencies {
   lazy val sparkVersion: String = sys.props.get("spark.version").getOrElse("3.5.0")
   lazy val hadoopVersion: String = sys.props.get("hadoop.version").getOrElse("3.3.4")
-  lazy val deltaVersion: String = "3.0.0"
+  lazy val deltaVersion: String = "3.1.0"
 
   val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion
