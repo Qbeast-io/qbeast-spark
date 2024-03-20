b06addbe1d47a33c2f1143024437cc3f49ddbd5d
osopardo1 <paola@qbeast.io>
2024-03-20 08:21:19 +0100

    Change CreateTableCommand for writing an empty dataframe with options and schema

--- src/main/scala/io/qbeast/spark/internal/sources/catalog/QbeastCatalog.scala
+++ src/main/scala/io/qbeast/spark/internal/sources/catalog/QbeastCatalog.scala
@@ -108,6 +108,8 @@ class QbeastCatalog[T <: TableCatalog with SupportsNamespaces with FunctionCatal
       properties: util.Map[String, String]): Table = {
 
     if (QbeastCatalogUtils.isQbeastProvider(properties)) {
+      // TODO check the schema
+      println("SCHEMA: " + schema)
       // Create the table
       QbeastCatalogUtils.createQbeastTable(
         ident,
@@ -119,6 +121,10 @@ class QbeastCatalog[T <: TableCatalog with SupportsNamespaces with FunctionCatal
         TableCreationMode.CREATE_TABLE,
         tableFactory,
         spark.sessionState.catalog)
+
+      // TODO check tables in the catalog
+      spark.sql("SHOW TABLES").show()
+
       // Load the table
       loadTable(ident)
     } else {
