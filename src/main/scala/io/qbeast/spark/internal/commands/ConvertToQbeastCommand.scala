0065bf7f268d96e6d33bdb1df637abcb059485cd
JosepSampe <josep.sampe@gmail.com>
2024-09-13 12:08:17 +0200

    Update scalafmt

--- src/main/scala/io/qbeast/spark/internal/commands/ConvertToQbeastCommand.scala
+++ src/main/scala/io/qbeast/spark/internal/commands/ConvertToQbeastCommand.scala
@@ -16,8 +16,8 @@
 package io.qbeast.spark.internal.commands
 
 import io.qbeast.core.model._
-import io.qbeast.spark.delta.DeltaQbeastSnapshot
 import io.qbeast.spark.delta.DeltaMetadataManager
+import io.qbeast.spark.delta.DeltaQbeastSnapshot
 import io.qbeast.spark.utils.MetadataConfig.lastRevisionID
 import io.qbeast.spark.utils.MetadataConfig.revision
 import io.qbeast.spark.utils.QbeastExceptionMessages.incorrectIdentifierFormat
