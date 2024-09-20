c678aec02b9cf98b6d1f8d9f03e6f4edd3951f79
osopardo1 <paola@qbeast.io>
2024-09-20 14:46:30 +0200

    Format

--- src/test/scala/io/qbeast/spark/index/model/transformer/CDFNumericQuantilesIndexingTest.scala
+++ src/test/scala/io/qbeast/spark/index/model/transformer/CDFNumericQuantilesIndexingTest.scala
@@ -65,7 +65,11 @@ class CDFNumericQuantilesIndexingTest
 
       val indexed = spark.read.format("qbeast").load(path)
       indexed.count() shouldBe 20
-      assertSmallDatasetEquality(df.union(df), indexed, ignoreNullable = true, orderedComparison = false)
+      assertSmallDatasetEquality(
+        df.union(df),
+        indexed,
+        ignoreNullable = true,
+        orderedComparison = false)
     })
 
   it should "trigger new revision when columnStats changes"
