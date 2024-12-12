45dee146db2c24e0c8bd808b144f05731d9992ca
jiawei <jiaweihu08@gmail.com>
2024-12-12 16:05:49 +0100

    Update Transformation tests

--- core/src/test/scala/io/qbeast/core/transform/CDFNumericQuantilesTransformationTest.scala
+++ core/src/test/scala/io/qbeast/core/transform/CDFNumericQuantilesTransformationTest.scala
@@ -7,50 +7,62 @@ import org.scalatest.matchers.should.Matchers
 
 class CDFNumericQuantilesTransformationTest extends AnyFlatSpec with Matchers {
 
-  val orderedDataTypeTest: IntegerDataType.type = IntegerDataType
-
-  "CDFNumericQuantilesTransformer" should "return correct transformation for found index" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    qt.transform(2) should be(0.5)
-  }
-
-  it should "return correct transformation for insertion point at start" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
+  "CDFNumericQuantilesTransformer" should "return transform correctly" in {
+    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1d, 2d, 3d), IntegerDataType)
     qt.transform(0) should be(0.0)
-  }
-
-  it should "return correct transformation for insertion point at end" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
+    qt.transform(1.5) should be(0.0)
+    qt.transform(2) should be(0.5)
     qt.transform(4) should be(1.0)
   }
 
-  it should "return correct transformation for insertion point in middle" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1.0, 2.0, 3.0), DoubleDataType)
-    qt.transform(1.5) should be(0.0)
+  it should "force quantiles to have more than 1 value" in {
+    an[IllegalArgumentException] should be thrownBy CDFNumericQuantilesTransformation(
+      Vector(1d),
+      IntegerDataType)
+
+    an[IllegalArgumentException] should be thrownBy CDFNumericQuantilesTransformation(
+      Vector(),
+      IntegerDataType)
   }
 
-  it should "return true when quantiles are different and neither is default" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    val newQt = CDFNumericQuantilesTransformation(IndexedSeq(4, 5, 6), orderedDataTypeTest)
-    qt.isSupersededBy(newQt) should be(true)
+  it should "be superseded by another Transformation" in {
+    val et = EmptyTransformation()
+    val ht = HashTransformation()
+    val idt = IdentityTransformation(0d, DoubleDataType)
+    val lt = LinearTransformation(-100d, 100d, DoubleDataType)
+    val cdf_st = CDFStringQuantilesTransformation(Vector("a", "b", "c"))
+    val cdf_nt = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.3), DoubleDataType)
+    val cdf_nt_2 = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.4), DoubleDataType)
+    val cdf_nt_3 = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.4), IntegerDataType)
+
+    cdf_nt.isSupersededBy(et) shouldBe false
+    cdf_nt.isSupersededBy(ht) shouldBe true
+    cdf_nt.isSupersededBy(idt) shouldBe true
+    cdf_nt.isSupersededBy(lt) shouldBe true
+    cdf_nt.isSupersededBy(cdf_st) shouldBe true
+    cdf_nt.isSupersededBy(cdf_nt) shouldBe false
+    cdf_nt.isSupersededBy(cdf_nt_2) shouldBe true
+    cdf_nt.isSupersededBy(cdf_nt_3) shouldBe true
   }
 
-  it should "return false when quantiles are the same and neither is default" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    val newQt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    qt.isSupersededBy(newQt) should be(false)
-  }
+  it should "merge with another Transformation" in {
+    val et = EmptyTransformation()
+    val ht = HashTransformation()
+    val idt = IdentityTransformation(0d, DoubleDataType)
+    val lt = LinearTransformation(-100d, 100d, DoubleDataType)
+    val cdf_st = CDFStringQuantilesTransformation(Vector("a", "b", "c"))
+    val cdf_nt = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.3), DoubleDataType)
+    val cdf_nt_2 = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.4), DoubleDataType)
+    val cdf_nt_3 = CDFNumericQuantilesTransformation(Vector(0.1, 0.2, 0.4), IntegerDataType)
 
-  "merge" should "return other transformation when it is a QuantileTransformation" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    val otherQt = CDFNumericQuantilesTransformation(IndexedSeq(4, 5, 6), orderedDataTypeTest)
-    qt.merge(otherQt) should be(otherQt)
-  }
-
-  it should "return current transformation when other is not a QuantileTransformation" in {
-    val qt = CDFNumericQuantilesTransformation(IndexedSeq(1, 2, 3), orderedDataTypeTest)
-    val other = new LinearTransformation(1, 10, 7, orderedDataTypeTest)
-    qt.merge(other) should be(other)
+    cdf_nt.merge(et) shouldBe cdf_nt
+    cdf_nt.merge(ht) shouldBe ht
+    cdf_nt.merge(idt) shouldBe idt
+    cdf_nt.merge(lt) shouldBe lt
+    cdf_nt.merge(cdf_st) shouldBe cdf_st
+    cdf_nt.merge(cdf_nt) shouldBe cdf_nt
+    cdf_nt.merge(cdf_nt_2) shouldBe cdf_nt_2
+    cdf_nt.merge(cdf_nt_3) shouldBe cdf_nt_3
   }
 
 }
